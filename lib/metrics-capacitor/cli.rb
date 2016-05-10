7c5328bd5c9271ccec163f542aaf1937e1e9392c
Radek blufor Slavicinsky <radek@blufor.cz>
2016-05-10 17:04:59 +0200

    work

--- lib/metrics-capacitor/cli.rb
+++ lib/metrics-capacitor/cli.rb
@@ -3,11 +3,10 @@ require_relative 'config'
 
 module MetricsCapacitor
   class CLI < Thor
+    include MetricsCapacitor::Process
 
-    @exit = false
-
-    $0 = 'metrics-capacitor'
     Config.load!
+
     package_name 'Metrics Capacitor'
 
     desc 'daemon', 'Run the workers'
@@ -21,19 +20,6 @@ module MetricsCapacitor
     def status
       Config.sidekiq_init_client!
       # TODO ...
-    end
-  end
-
-  def fork_child(*args, &block)
-    Process.fork do
-      %w(INT TERM KILL).each { |sig| Signal.trap(sig) { @exit = true } }
-      yield block
-    end
-  end
-
-  def fork_sidekiq(*args, &block)
-    Process.fork do
-      %w(INT TERM KILL).each do |sig| Signal.trap(sig) { exit 0 } }
     end
   end
 end
---
+++ lib/metrics-capacitor/process.rb
@@ -0,0 +1,31 @@
+module MetricsCapacitor
+  class Process
+
+    def initialize
+      @exit_flag = false
+      @pids = [ Process.pid ]
+    end
+
+    $0 = 'metrics-capacitor'
+
+    class << self
+
+      def fork_child(*args, &block)
+        Process.fork do
+          %w(INT TERM KILL).each { |sig| Signal.trap(sig) { @exit = true } }
+          $0 = "metrics-capacitor [#{args[:name]}]" if args[:name]
+          yield block
+        end
+      end
+
+      def run_sidekiq(*args)
+        Process.fork do
+          %w(INT TERM KILL).each do |sig| Signal.trap(sig) { exit 0 } }
+          $0 = "metrics-capacitor [sidekiq]"
+        end
+      end
+
+    end
+
+  end
+end
